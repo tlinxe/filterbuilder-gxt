ed339ca502d58e7e5d2309ea2c59b982c44d7b77
bekesizoltan <bekesizoltan@f18e3e16-0ab3-11df-beaf-bb271b2086c6>
2010-01-30 13:55:27 +0000

    adding icons

---
+++ src/hu/bekesi/zoltan/filterBuilder/client/icons/FilterBuilderIcons.java
@@ -0,0 +1,14 @@
+package hu.bekesi.zoltan.filterBuilder.client.icons;
+
+import com.google.gwt.user.client.ui.AbstractImagePrototype;
+import com.google.gwt.user.client.ui.ImageBundle;
+
+public interface FilterBuilderIcons extends ImageBundle {
+
+	@Resource("add.gif")
+	AbstractImagePrototype add();
+
+	@Resource("delete.gif")
+	AbstractImagePrototype delete();
+	
+}
